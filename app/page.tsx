212de6ed468d55b5f2641d51835e571617f39a80
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-08-15 19:54:25 -0300

    wip: homepage and globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <section className="container m-auto flex flex-col justify-center items-center active_border">
-      <div className="hero_image_vector">
+    <section className="p-5 app_container m-auto flex flex-col lg:flex-row-reverse justify-center gap-10 items-center">
+      <div className="hero_image_vector max-w-xl">
         <Image
           src="/assets/images/hero-image-colored.png"
           alt="Ikaro Felix in a black shirt sitting with his arms crossed in front of him"
@@ -12,22 +12,44 @@ const Home = () => {
         />
       </div>
 
-      <div className="flex sm:flex-col gap-4">
+      <div className=" flex flex-col gap-10 max-w-3xl">
+        <div className="flex flex-col gap-3">
+          <h1 className="2xl:text-6xl md:text-4xl text-2xl font-bold font-cabin">
+            <span className="relative">
+              <span className="before:absolute before:content-[''] before:left-0 before:bg-blue_color before:h-1 before:w-[95%] before:bottom-0 before:rounded">
+                Front-End{" "}
+              </span>
+            </span>
+            Developer
+          </h1>
+          <p className="2xl:text-xl md:text-base text-sm font-roboto text-justify">
+            Hi, I'm <span className="text-blue_color">Ikaro Felix</span>. An enthusiast in the art
+            of problem-solving and crafting creations from the ground up, situated in the state of
+            Minas Gerais in Brazil.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="2xl:text-4xl md:text-2xl text-xl font-bold font-cabin">Stacks</h2>
+        </div>
+      </div>
+
+      <div className="min-w-[32px] flex lg:flex-col lg:gap-8 gap-4">
         <a href="mailto:ikaroisdm@gmail.com" target="_blank" rel="noopener noreferrer">
-          <Image src="/assets/icons/gmail-icon.svg" alt="Gmail Icon" width={32} height={32} />
+          <Image src="/assets/icons/gmail-icon.svg" alt="Gmail Icon" width={35} height={35} />
         </a>
         <a href="https://linkedin.com/in/ikarofelix" target="_blank" rel="noopener noreferrer">
-          <Image src="/assets/icons/linkedin-icon.svg" alt="LinkedIn Icon" width={32} height={32} />
+          <Image src="/assets/icons/linkedin-icon.svg" alt="LinkedIn Icon" width={35} height={35} />
         </a>
         <a
           href="https://api.whatsapp.com/send/?phone=%2B5531998056550&text=Oi,%20vim%20pelo%20seu%20Portfolio"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image src="/assets/icons/whatsapp-icon.svg" alt="WhatsApp Icon" width={32} height={32} />
+          <Image src="/assets/icons/whatsapp-icon.svg" alt="WhatsApp Icon" width={35} height={35} />
         </a>
         <a href="https://github.com/ikarofelix" target="_blank" rel="noopener noreferrer">
-          <Image src="/assets/icons/github-icon.svg" alt="GitHub Icon" width={32} height={32} />
+          <Image src="/assets/icons/github-icon.svg" alt="GitHub Icon" width={35} height={35} />
         </a>
       </div>
     </section>
