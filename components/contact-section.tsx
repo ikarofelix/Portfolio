e7ad83c778f6e945a5cdcfda39e310f3a11c0eb4
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-08-26 15:20:04 -0300

    feat: add translation and SocialIcon implementation to contact-section

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -1,114 +1,47 @@
-import Image from "next/image";
+import { useTranslation } from "react-i18next";
+import { SocialIcon } from "./social-icons";
 
-export const ContactSection = () => {
+export const ContactSection: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="contact" className="pt-10 pb-10 w-full bg-lighter_color">
       <div className="app_container m-auto">
         <span className="relative w-fit">
-          <span className="section_title">Contact</span>
+          <span className="section_title">{t("contact.title")}</span>
         </span>
 
-        <div className="mt-5 w-fit m-auto grid lg:grid-rows-2 sm:grid-rows-3 grid-rows-6 justify gap-10 grid-flow-col">
-          <a
-            className="w-fit flex items-center gap-2"
-            href="https://www.google.com/maps/place/Minas+Gerais/@-18.5543766,-48.0922185,7z/data=!3m1!4b1!4m6!3m5!1s0xa690a165324289:0x112170c9379de7b3!8m2!3d-17.930178!4d-43.7908453!16zL20vMDFnaDZ6?entry=ttu"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              loading="lazy"
-              src="/assets/icons/socials/location-icon.svg"
-              alt="Location Icon"
-              title="Location"
-              width={35}
-              height={35}
-            />
-
-            <div>
-              <h1 className="font-bold text-lg">Location</h1>
-              <span>Minas Gerais, Brazil</span>
-            </div>
-          </a>
-          <a
-            className="w-fit flex items-center gap-2"
-            href="https://api.whatsapp.com/send/?phone=%2B5531998056550&text=Oi,%20vim%20pelo%20seu%20Portfolio"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              loading="lazy"
-              src="/assets/icons/socials/whatsapp-icon.svg"
-              alt="WhatsApp Icon"
-              title="WhatsApp"
-              width={35}
-              height={35}
-            />
-
-            <div>
-              <h1 className="font-bold text-lg">WhatsApp</h1>
-              <span>+55 (31) 99805-6550</span>
-            </div>
-          </a>
-          <a
-            className="w-fit flex items-center gap-2"
-            href="http://linkedin.com/in/ikarofelix"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              loading="lazy"
-              src="/assets/icons/socials/linkedin-icon.svg"
-              alt="LinkedIn Icon"
-              title="LinkedIn"
-              width={35}
-              height={35}
-            />
-
-            <div>
-              <h1 className="font-bold text-lg">LinkedIn</h1>
-              <span>/in/ikarofelix</span>
-            </div>
-          </a>
-          <a
-            className="w-fit flex items-center gap-2"
-            href="http://github.com/ikarofelix"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              loading="lazy"
-              src="/assets/icons/socials/github-icon.svg"
-              alt="GitHub Icon"
-              title="GitHub"
-              width={35}
-              height={35}
-            />
-
-            <div>
-              <h1 className="font-bold text-lg">GitHub</h1>
-              <span>/ikarofelix</span>
-            </div>
-          </a>
-          <a
-            className="w-fit flex items-center gap-2"
-            href="mailto:ikaroisdm@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              loading="lazy"
-              src="/assets/icons/socials/gmail-icon.svg"
-              alt="Gmail Icon"
-              title="Gmail"
-              width={35}
-              height={35}
-            />
-
-            <div>
-              <h1 className="font-bold text-lg">Gmail</h1>
-              <span className="break-all">ikaroisdm@gmail.com</span>
-            </div>
-          </a>
+        <div className="mt-5 w-fit m-auto grid lg:grid-rows-2 sm:grid-rows-3 grid-rows-5 justify gap-10 grid-flow-col">
+          <SocialIcon
+            icon="Location"
+            link="https://www.google.com/maps/place/Minas+Gerais/@-18.5543766,-48.0922185,7z/data=!3m1!4b1!4m6!3m5!1s0xa690a165324289:0x112170c9379de7b3!8m2!3d-17.930178!4d-43.7908453!16zL20vMDFnaDZ6?entry=ttu"
+            h1={t("contact.location")}
+            span={t("contact.address")}
+          />
+          <SocialIcon
+            icon="WhatsApp"
+            link="https://api.whatsapp.com/send/?phone=%2B5531998056550&text=Oi,%20vim%20pelo%20seu%20Portfolio"
+            h1="WhatsApp"
+            span="+55 (31) 99805-6550"
+          />
+          <SocialIcon
+            icon="LinkedIn"
+            link="http://linkedin.com/in/ikarofelix"
+            h1="LinkedIn"
+            span="/in/ikarofelix"
+          />
+          <SocialIcon
+            icon="GitHub"
+            link="http://github.com/ikarofelix"
+            h1="GitHub"
+            span="/ikarofelix"
+          />
+          <SocialIcon
+            icon="Gmail"
+            link="mailto:ikaroisdm@gmail.com"
+            h1="Gmail"
+            span="ikaroisdm@gmail.com"
+          />
         </div>
       </div>
     </section>
