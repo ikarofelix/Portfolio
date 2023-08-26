4f0ee7ccfd214a0a322361b5b0bd1d46df56b817
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-08-26 15:43:32 -0300

    refactor: layout and pages structure and add [lang] folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,49 @@
-import "@styles/globals.css";
-import { Nav } from "@components/navbar";
+"use client";
 
-export const metadata = {
-  title: "Ikaro Felix - Portfolio",
-  description: "Front-End Developer Portfolio by Ikaro Felix",
-};
+import "@styles/globals.css";
+import i18n from "./i18n";
+import { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const route = useRouter();
+  const pathName = usePathname();
+  const currentLanguage = i18n.language;
+
+  const getLang = () => {
+    if (currentLanguage === "pt" || pathName === "/pt") {
+      return "pt-BR";
+    } else {
+      return "en";
+    }
+  };
+
+  useEffect(() => {
+    if (pathName === "/en" || pathName === "/pt") {
+      i18n.changeLanguage(pathName.slice(1));
+    } else {
+      const userLang = navigator.language.split("-")[0];
+      i18n.changeLanguage(userLang);
+      route.push(`/${userLang}`);
+    }
+  }, []);
+
   return (
-    <html lang="en" className="bg-dark_color text-white_color">
+    <html lang={getLang()} className="bg-dark_color text-white_color">
+      <title>Ikaro Felix - Portfolio</title>
+      <meta
+        name="description"
+        content={`${
+          getLang() === "en"
+            ? "Front-End Developer's Portfolio by Ikaro Felix"
+            : "Portfólio de Ikaro Felix - Desenvolvedor Front-End"
+        }`}
+      />
       <body>
         <div className="page_background sm:after:block after:hidden">
           <div className="background_gradient sm:block hidden"></div>
         </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <main className="app">{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-import { HeroSection } from "@components/hero-section";
-import { AboutSection } from "@components/about-section";
-import { ProjectsSection } from "@components/projects-section";
-import { ContactSection } from "@components/contact-section";
-
 const Home = () => {
   return (
-    <div>
-      <HeroSection />
-      <AboutSection />
-      <ProjectsSection />
-      <ContactSection />
-      <footer className="w-full p-5 bg-footer_background_color m-auto text-lg text-center">
-        © 2023 Ikaro Felix
-      </footer>
+    <div className="h-screen w-screen flex items-center">
+      <div className="loading_spinner"></div>
     </div>
   );
 };
