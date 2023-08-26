36241d52cda6f7a55048cecab69882eff59e4e62
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-08-25 22:17:36 -0300

    fix: i18n bugs

--- app/i18n.js
+++ app/i18n.js
@@ -1,13 +1,9 @@
 import i18n from "i18next";
-import detector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
 import homeEN from "../public/locales/en/home.json";
 import homePT from "../public/locales/pt/home.json";
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
   en: {
     translation: homeEN,
@@ -17,22 +13,12 @@ const resources = {
   },
 };
 
-i18n
-  .use(detector)
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    resources,
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-    // ns: ["home"],
-    // defaultNS: "home",
-
-    keySeparator: false, // we do not use keys in form messages.welcome
-
-    interpolation: {
-      escapeValue: false, // react already safes from xss
-    },
-  });
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export default i18n;
