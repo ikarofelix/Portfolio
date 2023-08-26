738663fb888bf69a3c24cc4e3d04e6e7e129f0e5
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-08-26 12:17:41 -0300

    feat: add about-section translation

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -1,33 +1,20 @@
-export const AboutSection = () => {
+import { useTranslation } from "react-i18next";
+
+export const AboutSection: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <section id="about" className="pt-20 pb-20 w-full bg-lighter_color font-cabin">
       <div className="app_container m-auto h-full flex lg:flex-row flex-col">
         <div className="lg:w-[100%] flex flex-col gap-10">
           <span className="relative w-fit">
-            <span className="section_title">About Me</span>
+            <span className="section_title">{t("about.title")}</span>
           </span>
-          <h3 className="font-bold 2xl:text-3xl md:text-2xl text-xl">
-            I code to drive impactful change, infusing passion and creativity into every digital
-            creation.
-          </h3>
+          <h3 className="font-bold md:text-2xl text-xl">{t("about.subtitle")}</h3>
           <span className="text_small font-roboto text-justify">
-            <p>
-              I'm super into tech and all the amazing stuff it can do. That's why I got into web
-              development – it's this awesome combo of being creative, solving problems, and always
-              picking up new things. What really gets me excited is making apps that you can
-              interact with, and they look really cool too. Like, they can actually make a
-              difference in people's lives.
-            </p>
+            <p>{t("about.description.one")}</p>
             <br />
-            <p>
-              I'm one of those people who's always curious about how things tick. Right now, I'm
-              really getting into how systems are put together, especially things like databases and
-              servers. I'm even thinking of getting more into Back-End development down the road.
-              I'm always on the lookout for new knowledge – I'm a total learning junkie. Challenges
-              keep me fired up, and I love starting from scratch and building stuff up. Oh, and by
-              the way, I'm pretty handy with JavaScript, TypeScript, React, Redux, Firebase, MySQL,
-              GraphQL, NodeJS and some other stuff.
-            </p>
+            <p>{t("about.description.two")}</p>
           </span>
         </div>
         <div className="w-full lg:flex justify-center hidden">
