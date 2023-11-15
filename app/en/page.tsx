62347fd8085266540daa52ef9bb28765db1ff798
Ikaro Felix <117465215+ikarofelix@users.noreply.github.com>
2023-11-15 10:22:01 -0300

    update: added components

--- app/en/page.tsx
+++ app/en/page.tsx
@@ -7,6 +7,9 @@ import { ProjectsSection } from "@components/projects-section";
 import { ContactSection } from "@components/contact-section";
 import { I18nextProvider } from "react-i18next";
 import i18n from "../i18n";
+import { Education } from "@components/education";
+import { Experience } from "@components/experience";
+import { Certificates } from "@components/certificates";
 
 const PortfolioPage = () => {
   return (
@@ -15,8 +18,11 @@ const PortfolioPage = () => {
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
+      <Experience />
+      <Education />
+      <Certificates />
       <ContactSection />
-      <footer className="w-full p-5 bg-footer_background_color m-auto text-base text-center">
+      <footer className="w-full p-5 shadow-navbar m-auto text-base text-center">
         © 2023 Ikaro Felix
       </footer>
     </I18nextProvider>
